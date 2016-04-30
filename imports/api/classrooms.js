0bffcd5e049b9977e4fd6482e8086b4495ce6684
dhrushilbadani <badanidhrushil@berkeley.edu>
2016-04-29 17:11:57 -0700

    Can now add students to the classes

--- imports/api/classrooms.js
+++ imports/api/classrooms.js
@@ -1,4 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Classrooms = new Mongo.Collection('classrooms');
+export const Students = new Mongo.Collection('students');
 Classrooms.insert({ name: 'demo class', location: 'Mumbai', noOfStudents: 5, owner: '-1', username: 'Dhrushil'});
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Classrooms } from '/imports/api/classrooms.js'
+import { Students } from '/imports/api/classrooms.js'
 import './body.html'
 //
 Template.body.helpers({
@@ -9,7 +10,7 @@ Template.body.helpers({
 });
 
 Template.body.events({
-  'submit form'(event) {
+  'submit .new-classroom'(event) {
     // Prevent default browser form submit
     event.preventDefault();
 
@@ -20,18 +21,44 @@ Template.body.events({
         location: target.classLocation.value,
         noOfStudents: target.noOfStudents.value,
     };
-    // Insert a task into the collection
+    // Insert a class into the collection
     console.log(newClass);
     Classrooms.insert({
         name: target.classroomName.value,
         location: target.classLocation.value,
-        noOfStudents: parseInt(target.noOfStudents.value),
+        noOfStudents: target.noOfStudents.value,
+        students: [],
         email: Meteor.user().emails[0].address,
         owner: Meteor.userId(),
         username: Meteor.user().username,
     });
-    console.log(Classrooms.find());
+    console.log(c);
     // Clear form
     $('.new-classroom')[0].reset();
   },
+
+  'submit .new-student'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    var newStudent = {
+        studentName: target.studentName.value,
+        classroomName: target.whichClass.value,
+    };
+    // Insert a student into the collection
+    console.log(target.studentName.value);
+    Students.insert({
+        studentName: target.studentName.value,
+        classroomName: target.whichClass.value,
+    });
+    // Inserting the student into the associated class
+    c = Classrooms.findOne({name: target.whichClass.value});
+    console.log(c._id);
+    Classrooms.update(c._id, {$inc: {noOfStudents: 1}, $push: {students: newStudent}});
+    // Clear form
+    $('.new-student')[0].reset();
+  },
+
 });
